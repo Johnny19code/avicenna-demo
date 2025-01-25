ec5528102bcf635c1bc552edba2dfff6b9ec75c1
Sai Chandan <116301244+Johnny19code@users.noreply.github.com>
2025-01-25 21:25:32 +0530

    com 6

---
+++ licenses-certifications.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="eng">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Avicenna International Medical University</title>
+    <meta name="description" content="Avicenna International Medical University">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/animate.min.css" />
+    <link rel="stylesheet" href="css/magnific-popup.css" />
+    <link rel="stylesheet" href="fontawesome/css/all.min.css" />
+
+    <link rel="stylesheet" href="css/dripicons.css" />
+    <link rel="stylesheet" href="css/slick.css" />
+    <link rel="stylesheet" href="css/meanmenu.css" />
+    <link rel="stylesheet" href="css/default.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/responsive.css" />
+</head>
+
+<body>
+
+    <!-- header -->
+    <?php include_once 'includes/header.php' ?>
+    <!-- header-end -->
+
+    <!-- main-area -->
+    <main>
+        <!-- breadcrumb-area -->
+        <section
+            class="breadcrumb-area d-flex p-relative align-items-center"
+            style="background-image: url(img/bg/bdrc-bg.png)">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-xl-12 col-lg-12">
+                        <div class="breadcrumb-wrap text-left">
+                            <div class="breadcrumb-title">
+                                <h2>Licenses Certifications</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="breadcrumb-wrap2">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">
+                                    Licenses Certifications
+                                </li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- breadcrumb-area-end -->
+
+        <!-- shop-area -->
+        <section class="shop-area pt-120 pb-120 p-relative" data-animation="fadeInUp animated" data-delay=".2s">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/07.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/08.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/09.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/10.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/11.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/12.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/13.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/14.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/15.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/16.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/17.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/18.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/19.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/20.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/21.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/22.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/23.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/24.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/25.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/26.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/27.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/28.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="courses-item mb-30 hover-zoomin">
+                            <div class="thumb fix">
+                                <img src="img/gallery/29.jpg" alt="contact-bg-an-01" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- shop-area-end -->
+
+
+
+
+    </main>
+    <!-- main-area-end -->
+
+    <!-- footer -->
+    <?php include_once 'includes/footer.php' ?>
+    <!-- footer-end -->
+
+    <!-- JS here -->
+    <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/ajax-form.js"></script>
+    <script src="js/paroller.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/js_isotope.pkgd.min.js"></script>
+    <script src="js/imagesloaded.min.js"></script>
+    <script src="js/parallax.min.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/jquery.scrollUp.min.js"></script>
+    <script src="js/jquery.meanmenu.min.js"></script>
+    <script src="js/parallax-scroll.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/element-in-view.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
